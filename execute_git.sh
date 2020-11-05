68856a51d4e242d4a05b11bfd839f0b6fc6842e8
wangmingco <wangming.co@hotmail.com>
2020-11-05 11:11:01 +0800

    定期提交

--- execute_git.sh
+++ execute_git.sh
@@ -1,5 +1,8 @@
+#/bin/bash
+
 cd ${HOME}/vim_config
 
+/usr/bin/git add -A
 /usr/bin/git commit -a -m "定期提交"
 /usr/bin/git pull
 /usr/bin/git push -u origin master
--- run.sh
+++
@@ -1,3 +0,0 @@
-crontab crontab_config
-
-crontab -l
